b6bf4bb3c54b934204a89eac78c79a2b50bf3ed1
Stuart Ross <stuartross@dogbert.home>
2013-01-14 21:57:05 +0000

    Enable changing the repo path used by gitfech (creates a config file in /etc)

--- packages/bin/gitfetch.lua
+++ packages/bin/gitfetch.lua
@@ -1,9 +1,28 @@
 -- Original version by Stuart Ross - 10/1/2013
 
-function loadRepoSettings()
+local gitfetch = {}
+
+function gitfetch:loadRepoSettings()
+  local handle = fs.open('/etc/_gitfetch','r')
+  if handle then
+    local line = handle.readLine()
+    if line then self.currentRepo = textutils.unserialize( line ) end
+    handle.close()
+    return
+  end
+  
+  self.currentRepo = {root='https://raw.github.com',user='StuartIanRoss',repo='ComputerCraft',branch='master'}
+  
+  handle = fs.open('/etc/_gitfetch','w')
+  if handle then
+    handle.writeLine(textutils.serialize(self.currentRepo))
+    handle.close()
+  end
+end
+  
 end
 
-function fetchFile(url, outputPath)
+function gitfetch:fetchFile(url, outputPath)
   if shell.run('/bin/fetch', url, outputPath) then
   elseif shell.run('fetch', url, outputPath) then
   else
@@ -14,31 +33,33 @@ function fetchFile(url, outputPath)
   return true
 end
 
-function buildUrl(repoPath, root, user, repo, branch)
-  root = root or 'https://raw.github.com'
-  user = user or 'StuartIanRoss'
-  repo = repo or 'ComputerCraft'
-  branch = branch or 'master'
+function gitfetch:buildUrl(repoPath)
+  local root = self.currentRepo.root or 'https://raw.github.com'
+  local user = self.currentRepo.user or 'StuartIanRoss'
+  local repo = self.currentRepo.repo or 'ComputerCraft'
+  local branch = self.currentRepo.branch or 'master'
   return root .. '/' .. user .. '/' .. repo .. '/' .. branch .. '/' .. repoPath
 end
 
-function printUsage()
+function gitfetch:printUsage()
   print('Usage:')
   print('gitfetch <repo path> <output path> : will fetch at <repo path> in the current repo and branch')
   print(' and save it in <target path>')
 end
 
-function main(args)
+function gitfetch:main(args)
   if #args ~= 2 then
-    printUsage()
+    self:printUsage()
     return
   end
+  
+  self:loadRepoSettings()
 	
   local repoPath = args[1]
 
-  local url = buildUrl(repoPath)
-  return fetchFile( url, args[2] )
+  local url = self:buildUrl(repoPath)
+  return self:fetchFile( url, args[2] )
 end
 
 local args = {...}
-return main(args)
+return gitfetch:main(args)
