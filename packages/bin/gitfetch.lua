4a32b20f0ee150c13a464510b2d4aa0d5234ef99
Stuart Ross <stuartross@dogbert.home>
2013-01-14 22:39:18 +0000

    Add libfetch, and change gitfetch to use it

--- packages/bin/gitfetch.lua
+++ packages/bin/gitfetch.lua
@@ -1,43 +1,10 @@
 -- Original version by Stuart Ross - 10/1/2013
 
-local gitfetch = {}
-
-function gitfetch:loadRepoSettings()
-  local handle = fs.open('/etc/_gitfetch','r')
-  if handle then
-    local line = handle.readLine()
-    if line then self.currentRepo = textutils.unserialize( line ) end
-    handle.close()
-    return
-  end
-  
-  self.currentRepo = {root='https://raw.github.com',user='StuartIanRoss',repo='ComputerCraft',branch='master'}
-  
-  handle = fs.open('/etc/_gitfetch','w')
-  if handle then
-    handle.writeLine(textutils.serialize(self.currentRepo))
-    handle.close()
-  end
+function require(api)
+  shell.run(api)
 end
 
-function gitfetch:fetchFile(url, outputPath)
-  if shell.run('/bin/fetch', url, outputPath) then
-  elseif shell.run('fetch', url, outputPath) then
-  else
-    print('Failed to find fetch, could not download file.')
-    return false
-  end
-  
-  return true
-end
-
-function gitfetch:buildUrl(repoPath)
-  local root = self.currentRepo.root or 'https://raw.github.com'
-  local user = self.currentRepo.user or 'StuartIanRoss'
-  local repo = self.currentRepo.repo or 'ComputerCraft'
-  local branch = self.currentRepo.branch or 'master'
-  return root .. '/' .. user .. '/' .. repo .. '/' .. branch .. '/' .. repoPath
-end
+require('libfetch')
 
 function gitfetch:printUsage()
   print('Usage:')
@@ -50,13 +17,8 @@ function gitfetch:main(args)
     self:printUsage()
     return
   end
-  
-  self:loadRepoSettings()
-	
-  local repoPath = args[1]
 
-  local url = self:buildUrl(repoPath)
-  return self:fetchFile( url, args[2] )
+  return libfetch.fetchViaGit( args[1], args[2] )
 end
 
 local args = {...}
---
+++ packages/lib/libfetch.lua
@@ -0,0 +1,62 @@
+-- libfetch
+--
+-- A collection of functions for getting files from the web
+-- Created by Stuart Ross 14/1/2013
+-- Generic fetch by William Pond
+-- Git fetch by Stuart Ross
+
+_LibFetch = {}
+
+_LibFetch.new = function()
+
+  local self = {}
+  
+  -- Generic
+  self.fetchFile = function(url, outputPath)
+    if shell.run('/bin/fetch', url, outputPath) then
+    elseif shell.run('fetch', url, outputPath) then
+    else
+      print('Failed to find fetch, could not download file.')
+      return false
+    end
+  
+    return true
+  end
+  -- End generic
+
+  -- Git
+  self._loadRepoSettings = function()
+    local handle = fs.open('/etc/_gitfetch','r')
+    if handle then
+      local line = handle.readLine()
+      if line then self.currentRepo = textutils.unserialize( line ) end
+      handle.close()
+      return
+    end
+  
+    self.currentRepo = {root='https://raw.github.com',user='StuartIanRoss',repo='ComputerCraft',branch='master'}
+  
+    handle = fs.open('/etc/_gitfetch','w')
+    if handle then
+      handle.writeLine(textutils.serialize(self.currentRepo))
+      handle.close()
+    end
+  end
+
+  self._buildGitUrl = function(repoPath)
+    local root = self.currentRepo.root or 'https://raw.github.com'
+    local user = self.currentRepo.user or 'StuartIanRoss'
+    local repo = self.currentRepo.repo or 'ComputerCraft'
+    local branch = self.currentRepo.branch or 'master'
+    return root .. '/' .. user .. '/' .. repo .. '/' .. branch .. '/' .. repoPath
+  end
+
+  self.fetchViaGit = function(repoPath,localPath)
+    self._loadRepoSettings()
+    local url = self._buildGitUrl(repoPath)
+    return self._fetchFile( url, args[2] )
+  end
+  -- End Git
+end
+
+libfetch = _LibFetch.new()
