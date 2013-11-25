3a9078f8bf12f5dd48a49ab05a26a740aa73d433
wpond <w.pond@sms.ed.ac.uk>
2013-11-25 19:30:53 +0000

    runner update

--- packages/usr/greyeric/runner.lua
+++ packages/usr/greyeric/runner.lua
@@ -9,14 +9,29 @@ require("/lib/String")
 
 args = {...}
 
+B1 = Vector.parseString("-45,72,-131")
+B2 = Vector.parseString("-50,72,-131")
+
 function main()
 	
-	if not #args == 2 then
+	if #args == 0 then
 		print("Location and destinations not specified")
 		error()
 	end
 	
-	print("Moving to/from...")
+	local myPos = Vector.parseString(args[1])
+	local height = 250
+	
+	local dest = args[2]
+	
+	local B1Diff = Vector(B1.x - myPos.x, B1.y - myPos.y, B1.z - myPos.z)
+	local B2Diff = Vector(B2.x - myPos.x, B2.y - myPos.y, B2.z - myPos.z)
+	
+	if (dest == "home") then
+		Agent.move(B1Diff)
+	else
+		Agent.move(B2Diff)
+	end
 	
 end
 
