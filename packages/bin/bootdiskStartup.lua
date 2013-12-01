ffbb13ec0e31e6149d4e786850ccae8914dfe282
Harry Gordon <harrygordon1000@googlemail.com>
2013-12-01 12:42:34 +0000

    Added some flourishes to the startup script: 1) script copies itself to the root drive so you don't need the boot disk after installation, 2) added some sick ascii art, 3) added various comments

--- packages/bin/bootdiskStartup.lua
+++ packages/bin/bootdiskStartup.lua
@@ -1,10 +1,38 @@
+-- Pew OS startup script
+-- Originally by Stuart Ross, 12/01/2013
+-- Modified by Harry Gordon, 01/12/2013
+
+-- Artwork for start up
+local art = 
+  "\n\n\n\n\n\n\n" ..
+  "   __________              ________    _________ \n" ..
+  "   \\______   \\ ______  _  _\\_____  \\  /   _____/ \n" ..
+  "    |     ___// __ \\ \\/ \\/ //   |   \\ \\_____  \\ \n" ..
+  "    |    |   \\  ___/\\     //    |    \\/        \\ \n" ..
+  "    |____|    \\___  >\\/\\_/ \\_______  /_______  / \n" ..
+  "                  \\/               \\/        \\/ \n" ..
+  "\n\n\n\n\n\n\n"
+
+-- Create required folders
 fs.makeDir('/bin')
 fs.makeDir('/etc')
 fs.makeDir('/lib')
 fs.makeDir('/opt')
 fs.makeDir('/usr')
 
+-- Move libfetch, ppt and startup script
 if not fs.exists('/lib/libfetch') then fs.copy('/disk/libfetch','/lib/libfetch') end
 if not fs.exists('/bin/ppt') then fs.copy('/disk/ppt','/bin/ppt') end
+if not fs.exists('/startup') then fs.copy('/disk/startup','/startup') end
+
+-- Set up ppt (not sure how this works - HG)
 shell.run('/bin/ppt', 'get ppt')
+
+-- Get makeBootDisk
 shell.run('ppt', 'get makeBootDisk')
+
+-- Finish up with some art and clear
+os.sleep(1)
+print(art)
+os.sleep(1)
+shell.run("clear")
